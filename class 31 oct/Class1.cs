f51a60cb746c394b5cd4332455d9c686636d4235
akashkuchankar <kuchankarakash@gmail.com>
2022-10-31 11:44:26 -0700

    Add project files.

---
+++ class 31 oct/Class1.cs
@@ -0,0 +1,43 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace class_31_oct
+//{
+//    public class MyGeneric<T>
+//    { 
+//        private T data;
+//        public string value;
+//        public void Accept(T data)
+//        {
+//            this.data = data;
+//        }
+
+//        public T GetData()
+//        {
+//            return data;
+//        }
+//    }
+//    public class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            MyGeneric<int> myGeneric1 = new MyGeneric<int>();
+//            myGeneric1.Accept(10);
+
+//           // MyGeneric<Employee> myGeneric2 = new MyGeneric<Employee>();
+//           // myGeneric2.Accept(new Employee("test", 20000));
+
+//            MyGeneric<string> s = new MyGeneric<string>();
+//            s.Accept("Hello");
+
+
+//        }
+
+
+//    }
+
+//}
+
---
+++ class 31 oct/Product.cs
@@ -0,0 +1,56 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace class_31_oct
+//{
+//    internal class Product
+//    {
+//        private int price;
+//        private string name;
+//        public Product(int price, string name)
+//        {
+//            this.price = price;
+//            this.name = name;
+//        }
+//        public override string ToString()
+//        {
+//            return "Product :" + price + "  " + name;
+//        }
+//    }
+
+//    public class Equipment : IEnumerable
+//    {
+//        private Product[] p;
+//        public Equipment()
+//        {
+//            p = new Product[3];
+//            p[0] = new Product(234, "Vcd");
+//            p[1] = new Product(133, "tv");
+//            p[2] = new Product(11000, "DVD");
+//        }
+
+//        public IEnumerator GetEnumerator()
+//        {
+//            return p.GetEnumerator();
+//        }
+//    }
+
+//    public class Program
+//    {
+
+//        static void Main(string[] args)
+//        {
+
+//            Equipment team = new Equipment();
+//            foreach (var item in team)
+//            {
+//                Console.WriteLine(item);
+//            }
+//        }
+
+//    }
+//}
